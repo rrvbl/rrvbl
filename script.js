f38556935b41b5f50ab35fee25ee9ffc13c5b253
EvanDoesGitHub <76410162+EvanDoesGitHub@users.noreply.github.com>
2025-07-07 12:17:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Initialize Supabase client
-const SUPABASE_URL = 'YOUR_SUPABASE_URL'; // Replace with your Supabase Project URL
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY'; // Replace with your Supabase Public Anon Key
+const SUPABASE_URL = 'https://kkekhqkjucnzvlmsbest.supabase.co'; // Replace with your Supabase Project URL
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtrZWtocWtqdWNuenZsbXNiZXN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5MDE3NzIsImV4cCI6MjA2NzQ3Nzc3Mn0.ljdgW8Vawn01lozpRdD0YFKczExfAfO5-TDGdkNvAMQ'; // Replace with your Supabase Public Anon Key
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
